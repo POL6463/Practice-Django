a6aa1693b3ad58a43e6db738a4f71c06092c998b
POL6463 <kevinkim0617kim@gmail.com>
2021-08-03 00:57:57 +0900

    Add url list pages

--- ch99/bookmark/views.py
+++ ch99/bookmark/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.views.generic import ListView, DetailView
+from bookmark.model import Bookmark
+
+
+class BookmarkLV(ListView):
+    model = Bookmark
+
+
+class BookmarkDV(DetailView):
+    model = Bookmark
